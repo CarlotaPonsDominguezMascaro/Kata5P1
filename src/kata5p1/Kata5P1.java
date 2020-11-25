f60590f418ee6d920de2b2ee30eeb2693e3c3409
Carlota Pons <carlotaponsdm@gmail.com>
2020-11-25 09:34:24 +0000

    Kata 5 P1 version3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -5,10 +5,13 @@
  */
 package kata5p1;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 /**
  *
@@ -19,20 +22,42 @@ public class Kata5P1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    private static final String filename = "email.txt";
+    private static List<String> mailList;
+    private final static String url = "jdbc:sqlite:/Users/carlotapons/Documents/Universidades/4_curso/Is2/Kata5P1/Kata5.db";
+    private static Connection conn;
+
+    public static void main(String[] args) throws IOException, SQLException {
         // TODO code application logic here
+        
+        ConectarYCrear();
+        mailList = MailListReader.read(filename);
+        String sql = "INSERT INTO direcc_email(Mail) VALUES(?)";
+        
+        try (Connection conn = DriverManager.getConnection(url); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (int i = 0; i < mailList.size(); i++) {
+
+                pstmt.setString(1, mailList.get(i));
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void ConectarYCrear() throws IOException {
         ConectarBD con = new ConectarBD();
         con.selectAll();
-        try(Connection conn = DriverManager.getConnection( "jdbc:sqlite:/Users/carlotapons/Documents/Universidades/4_curso/Is2/Kata5P1/Kata5.db"); 
-                Statement stmt = conn.createStatement()){
+        try ( Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/carlotapons/Documents/Universidades/4_curso/Is2/Kata5P1/Kata5.db");  Statement stmt = conn.createStatement()) {
             stmt.execute("CREATE TABLE IF NOT EXISTS direcc_email "
                     + "(\n" + " id integer PRIMARY KEY AUTOINCREMENT,\n"
                     + " Mail text NOT NULL);"
-                                );
-            
-        }catch(SQLException e){
+            );
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
 }
